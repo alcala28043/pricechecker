de4ee4a523fbf4eae744ab14076952814b790f05
Arturo Alcalá <alcala28043@gmail.com>
2025-06-22 10:53:57 +0200

    feat: initialize Spring Boot project with basic dependencies

---
+++ src/test/java/dev/kpucha/pricechecker/PricecheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kpucha.pricechecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricecheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
